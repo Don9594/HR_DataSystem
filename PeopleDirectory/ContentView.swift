b458a37659986a8b66fdf668616d9394a25c9efd
Don Wijesinghe <menuwan.wijesinghe@gmail.com>
2022-03-29 18:47:17 -0500

    Initial Commit

---
+++ PeopleDirectory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PeopleDirectory
+//
+//  Created by Don wijesinghe on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PeopleDirectory/PeopleDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleDirectoryApp.swift
+//  PeopleDirectory
+//
+//  Created by Don wijesinghe on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PeopleDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
